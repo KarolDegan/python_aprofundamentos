b74216f806123e453613ea3f8370b06e4e093321
Karol <karol.degan@hotmail.com>
2025-05-19 17:50:37 -0300

    Aula 1 atualizada

---
+++ fastapi/aula1.py
@@ -0,0 +1,4 @@
+# O pip é usado para instalar bibliotecas Python que você usa dentro de um programa Python.
+# O pipx é usado para instalar ferramentas feitas em Python que você usa no terminal, como programas de linha de comando.
+#  Poetry é um gerenciador de projetos para Python.
+#       poetry new nome_pacote: já estrutura um pacote com confs iniciais
